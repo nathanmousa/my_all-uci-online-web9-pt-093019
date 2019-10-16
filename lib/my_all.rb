3432aabf6f6c740d97dd9a7122de482eab5d0b6c
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-16 23:35:01 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  stored = []
+  
+  while i < collection.length
+    stored << yield(collection[i])
+    i += 1
+  end
+  if stored.include?(false)
+    false
+  else
+    true
+  end
 end
+
+my_all?([1,2,3]) {|i| i < 2}
